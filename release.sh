f59c3f6bf915000c673d1f2a1cb15f59b4064e59
rxchi1d <test@example.com>
2025-01-27 21:08:28 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -16,62 +16,62 @@ TODAY_DATE=$(date -u +"%Y-%m-%d")
 # 初始化資料夾
 ############################################
 
-# rm -rf $OUTPUT_DIR
-# mkdir -p $OUTPUT_DIR
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 
 ############################################
 # 下載 geoname_data 資料
 ############################################
 
-# echo "執行 prepare_geoname_data.sh..."
-# bash prepare_geoname_data.sh
-# if [[ $? -ne 0 ]]; then
-#     echo "執行 prepare_geoname_data.sh 失敗！退出。"
-#     exit 1
-# fi
+echo "執行 prepare_geoname_data.sh..."
+bash prepare_geoname_data.sh
+if [[ $? -ne 0 ]]; then
+    echo "執行 prepare_geoname_data.sh 失敗！退出。"
+    exit 1
+fi
 
 ############################################
 # 優化 geoname_data 資料
 ############################################
 
-# # 修改 admin1CodesASCII.txt 檔案以符合台灣慣用格式
-# echo "執行 python geodata/modify_admin1.py..."
-# python geodata/modify_admin1.py
-# if [[ $? -ne 0 ]]; then
-#     echo "執行 python geodata/modify_admin1.py 失敗！退出。"
-#     exit 1
-# fi
+# 修改 admin1CodesASCII.txt 檔案以符合台灣慣用格式
+echo "執行 python geodata/modify_admin1.py..."
+python geodata/modify_admin1.py
+if [[ $? -ne 0 ]]; then
+    echo "執行 python geodata/modify_admin1.py 失敗！退出。"
+    exit 1
+fi
 
-# # 根據 extra_data/TW.txt 優化 cities500.txt
-# echo "執行 python geodata/enhance_data.py..."
-# python geodata/enhance_data.py
-# if [[ $? -ne 0 ]]; then
-#     echo "執行 python geodata/enhance_data.py 失敗！退出。"
-#     exit 1
-# fi
+# 根據 extra_data/TW.txt 優化 cities500.txt
+echo "執行 python geodata/enhance_data.py..."
+python geodata/enhance_data.py
+if [[ $? -ne 0 ]]; then
+    echo "執行 python geodata/enhance_data.py 失敗！退出。"
+    exit 1
+fi
 
 ############################################
 # 翻譯 geoname_data 資料
 ############################################
 
-# # 利用 locationiq API 取得 metadata
-# LIST=("TW")
-# for item in "${LIST[@]}"; do
-#     echo "執行 python geodata/generate_geodata_locationiq.py --country_code $item..."
-#     python geodata/generate_geodata_locationiq.py --country_code "$item"
-#     if [[ $? -ne 0 ]]; then
-#         echo "執行 python geodata/generate_geodata_locationiq.py --country_code  $item 失敗！退出。"
-#         exit 1
-#     fi
-# done
+# 利用 locationiq API 取得 metadata
+LIST=("TW")
+for item in "${LIST[@]}"; do
+    echo "執行 python geodata/generate_geodata_locationiq.py --country_code $item..."
+    python geodata/generate_geodata_locationiq.py --country_code "$item"
+    if [[ $? -ne 0 ]]; then
+        echo "執行 python geodata/generate_geodata_locationiq.py --country_code  $item 失敗！退出。"
+        exit 1
+    fi
+done
 
-# # 翻譯 cities500_en.txt, admin1CodesASCII_en.txt, admin2Codes_en.txt
-# echo "執行 python geodata/translate.py..."
-# python geodata/translate.py
-# if [[ $? -ne 0 ]]; then
-#     echo "執行 python geodata/translate.py 失敗！退出。"
-#     exit 1
-# fi
+# 翻譯 cities500_en.txt, admin1CodesASCII_en.txt, admin2Codes_en.txt
+echo "執行 python geodata/translate.py..."
+python geodata/translate.py
+if [[ $? -ne 0 ]]; then
+    echo "執行 python geodata/translate.py 失敗！退出。"
+    exit 1
+fi
 
 ############################################
 # 產生 release 檔案
