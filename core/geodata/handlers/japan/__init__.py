ebf240232474dbc6548958373e4fbd26c2d27afb
RxChi1d <rchildlin@gmail.com>
2026-06-01 16:02:45 +0800

    refactor(migration): 移除 Python production 路徑

--- core/geodata/handlers/japan/__init__.py
+++
@@ -1,5 +0,0 @@
-"""日本 handler 套件。"""
-
-from core.geodata.handlers.japan.handler import JapanGeoDataHandler
-
-__all__ = ["JapanGeoDataHandler"]
--- core/geodata/handlers/south_korea/__init__.py
+++
@@ -1,5 +0,0 @@
-"""南韓 handler 套件。"""
-
-from core.geodata.handlers.south_korea.handler import SouthKoreaGeoDataHandler
-
-__all__ = ["SouthKoreaGeoDataHandler"]
